b572e748602fb6dea099481de0672a115a570d2f
Sayed Abdul <sakarim9124@gmail.com>
2025-01-26 21:31:29 +0530

    Add book model and retrieval functions to manage book data

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-def generateName(firstname: str, lastname):
+from typing import List, Optional
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: Optional[int] = None
+
+# Sample data
+books = [
+    Book(id=1, title="The Great Gatsby", author="F. Scott Fitzgerald", year=1925),
+    Book(id=2, title="1984", author="George Orwell", year=1949)
+]
+
+def get_all_books() -> List[Book]:
+    """Get all books from the database"""
+    return books
+
+def get_book_by_id(book_id: int) -> Optional[Book]:
+    """Get a book by its ID"""
+    return next((book for book in books if book.id == book_id), None)
+
+def generateName(firstname: str, lastname: str) -> str:
+    """Generate full name"""
     return firstname.capitalize() + " " + lastname
 
 
